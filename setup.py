4169de2a15503266b976f966eb2e02b85f054935
jonathanGR3450 <52362305+jonathanGR3450@users.noreply.github.com>
2020-06-08 11:06:27 -0500

    Feature/pip-config (#1)
    * update of structure and setup
    
    Co-authored-by: jonathan <garzon>

--- setup.py
+++ setup.py
@@ -34,20 +34,18 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name = 'djangoplicity-actions',
-    version = '0.1',
-    packages = find_packages('src'),
-    package_dir = { '': 'src' },
-    include_package_data = True,
-    zip_safe = False,
-    namespace_packages = ['djangoplicity'],
-    install_requires = ['setuptools'],
+    name='djangoplicity-actions',
+    version='0.2.0',
+    packages=find_packages(include=['djangoplicity', 'djangoplicity.*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
     
     # metadata for upload to PyPI
-    author = 'European Southern Observatory',
-    author_email = 'information@eso.org',
-    description = 'Djangoplicity application for',
-    license = "New BSD License",
-    keywords = "django djangoplicity",
-    url = "http://www.djangoplicity.org/app/actions/"
+    author='European Southern Observatory',
+    author_email='information@eso.org',
+    description='Djangoplicity application for',
+    license="New BSD License",
+    keywords="django djangoplicity",
+    url="http://www.djangoplicity.org/app/actions/"
 )
