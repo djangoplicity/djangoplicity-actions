061e92f2e8cb622a68ceb0a27b0a75352a8b6f78
jonathan <garzon>
2020-06-17 15:37:43 -0500

    add test app

---
+++ test_app/tests.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from djangoplicity.actions.models import Action
+
+# Create your tests here.
+class ActionsTestCase(TestCase):
+
+    def test_action(self):
+        print ''
