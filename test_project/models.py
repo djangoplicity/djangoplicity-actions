16f342283d14e5b433efddd8452773d9a1575b78
jonathan <garzon>
2020-08-03 13:34:13 -0500

    add class and model to tests

--- test_project/models.py
+++ test_project/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from djangoplicity.actions.models import EventAction
 from djangoplicity.actions.plugins import ActionPlugin
+from django.contrib.contenttypes.models import ContentType
 
+
+class SomeModelTest(models.Model):
+    api_key = models.CharField(max_length=255, verbose_name="API key")
+    list_id = models.CharField(unique=True, max_length=50)
+    web_id = models.CharField(blank=True, max_length=255)
+    connected = models.BooleanField(default=False)
+    content_type = models.ForeignKey(ContentType, null=True, blank=True,
+        help_text='Select the content type of objects that subscribers on '
+        'this list can be linked with.')
 
 ACTION_EVENTS = (
     ('on_subscribe', 'On subscribe'),
@@ -11,18 +21,18 @@ ACTION_EVENTS = (
     ('on_cleaned', 'On cleaned'),
     ('on_campaign', 'On campaign'),
 )
-# class SomeEventAction(EventAction):
-#     '''
-#     Define actions to be executed when a event occurs for a list (e.g. sub,
-#     unsub, clean etc.)
-#     '''
-#     def __init__(self, *args, **kwargs):
-#         super(SomeEventAction, self).__init__(*args, **kwargs)
-#         self._meta.get_field('on_event')._choices = ACTION_EVENTS
+class SomeEventAction(EventAction):
+    '''
+    Define actions to be executed when a event occurs for a list (e.g. sub,
+    unsub, clean etc.)
+    '''
+    def __init__(self, *args, **kwargs):
+        super(SomeEventAction, self).__init__(*args, **kwargs)
+        self._meta.get_field('on_event')._choices = ACTION_EVENTS
 
-#     # model_object = models.ForeignKey(SomeList)
+    model_object = models.ForeignKey(SomeModelTest)
 
-#     _key = 'djangoplicity.mailinglists.action_cache'
+    _key = 'djangoplicity.mailinglists.action_cache'
 
 
 class SimpleAction( ActionPlugin ):
